6bd14e4b5464f1b0ce64253b38b31480f4eae4b2
Vishal A <itz.me.vishal.24@gmail.com>
2026-06-30 12:54:30 +0530

    CSS designed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -105,7 +105,7 @@ const ContactFunc = () => {
                 </p>
               </div>
             </div>
-
+<br />
             <div
               className="faq-section"
               style={{
